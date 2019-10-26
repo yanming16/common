9b78982b5b46e6e9a2a34d6eb3a758089da91ef5
liangyi <liangyi@qunhemail.com>
2019-10-26 12:18:12 +0800

    修缮httpUtils工具类，fix post请求异常问题

--- src/main/java/com/xiaoming/enums/HttpRequestMethodEnum.java
+++ src/main/java/com/xiaoming/enums/HttpRequestMethodEnum.java
@@ -1,9 +1,3 @@
-/*
- * HttpRequestMethodEnum.java
- * Copyright 2019 Qunhe Tech, all rights reserved.
- * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
- */
-
 package com.xiaoming.enums;
 
 import lombok.AllArgsConstructor;
